583285ed380cc761f3627c68661cea636b838436
Egor Shmelev <shmelevyegor25@gmail.com>
2023-12-26 00:38:45 -0300

    some fixes to functions.py

--- src/functions.py
+++ src/functions.py
@@ -1,14 +1,10 @@
 import json
-from config import OPERATIONS_PATH
 
 
 def load_all_operations(path):
     with open(path, encoding='utf-8') as file:
         operations = json.load(file)
         return operations
-
-
-operations = load_all_operations(OPERATIONS_PATH)
 
 
 def filter_operations(path):
