848b929f1f110f22434b1943d5532604992cbf20
Egor Shmelev <shmelevyegor25@gmail.com>
2023-12-25 22:38:47 -0300

    added filter_operations func

--- src/functions.py
+++ src/functions.py
@@ -1,7 +1,19 @@
 import json
+from config import OPERATIONS_PATH
 
 
 def load_all_operations(path):
     with open(path, encoding='utf-8') as file:
         operations = json.load(file)
         return operations
+
+
+operations = load_all_operations(OPERATIONS_PATH)
+
+
+def filter_operations(operations):
+    executed_operations = []
+    for operation in operations:
+        if operation.get('state') == 'EXECUTED':
+            executed_operations.append(operation)
+    return executed_operations
