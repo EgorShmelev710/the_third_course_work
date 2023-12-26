a8aaab77305dfa4c45e428b7529dd9e13922f888
Egor Shmelev <shmelevyegor25@gmail.com>
2023-12-26 01:16:49 -0300

    added sort_filtered_operations func

--- src/functions.py
+++ src/functions.py
@@ -13,3 +13,11 @@ def filter_operations(path):
         if operation.get('state') == 'EXECUTED':
             executed_operations.append(operation)
     return executed_operations
+
+
+def sort_filtered_operations(path):
+    """
+    функция выдает 5 последних совершенных операций
+    """
+    result = sorted(path, key=lambda path: path['date'], reverse=True)
+    return result[:5]
